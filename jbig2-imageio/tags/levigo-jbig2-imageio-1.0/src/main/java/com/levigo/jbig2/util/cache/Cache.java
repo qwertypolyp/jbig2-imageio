8832569bff2eb0fbc6fd85e77225e4b4f8800506
matthaeus.krzikalla <matthaeus.krzikalla@204cc359-37cf-974a-25db-4049523e438a>
2010-07-30 09:27:00 +0000

    [maven-release-plugin]  copy for tag levigo-jbig2-imageio-1.0

---
+++ jbig2-imageio/tags/levigo-jbig2-imageio-1.0/src/main/java/com/levigo/jbig2/util/cache/Cache.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 1995-2010 levigo holding gmbh.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.levigo.jbig2.util.cache;
+
+/**
+ * @author <a href="mailto:m.krzikalla@levigo.de">Matthäus Krzikalla</a>
+ */
+public interface Cache {
+
+
+  Object put(Object key, Object value, int sizeEstimate);
+
+  Object get(Object key);
+
+  /**
+   * Removes all mappings from a map (optional operation).
+   * 
+   * @throws UnsupportedOperationException if {@code clear()} is not supported by the map.
+   */
+  void clear();
+
+  Object remove(Object key);
+}
