2f11b019fc51eac5f9a8dfe2d4bb7a0f0fb6dfc7
matthaeus.krzikalla <matthaeus.krzikalla@204cc359-37cf-974a-25db-4049523e438a>
2012-09-12 09:00:55 +0000

    update to newest junit version; license header date updated

--- jbig2-imageio/trunk/src/main/java/com/levigo/jbig2/decoder/huffman/FixedSizeTable.java
+++ jbig2-imageio/trunk/src/main/java/com/levigo/jbig2/decoder/huffman/FixedSizeTable.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 1995-2010 levigo holding gmbh.
+ * Copyright (C) 1995-2012 levigo holding gmbh.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
