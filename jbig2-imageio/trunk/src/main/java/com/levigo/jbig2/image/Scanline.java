93b1a93ea738d841e9175ddf2ebd39de1bd17a52
matthaeus.krzikalla <matthaeus.krzikalla@204cc359-37cf-974a-25db-4049523e438a>
2012-09-13 10:04:51 +0000

    added license header to new files

--- jbig2-imageio/trunk/src/main/java/com/levigo/jbig2/image/Scanline.java
+++ jbig2-imageio/trunk/src/main/java/com/levigo/jbig2/image/Scanline.java
@@ -1,11 +1,20 @@
 /**
- * <pre>
- * Copyright (c) 1995-2012 levigo holding gmbh. All Rights Reserved.
+ * Copyright (C) 1995-2012 levigo holding gmbh.
  *
- * This software is the proprietary information of levigo holding gmbh.
- * Use is subject to license terms.
- * </pre>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.levigo.jbig2.image;
 
 import java.awt.image.MultiPixelPackedSampleModel;
